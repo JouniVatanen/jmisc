eedffb9daa0e9a1b6c04aa66f843488206ce72f8
Jouni Vatanen <jouni.vatanen@ilmarinen.fi>
2022-09-23 08:41:08 +0300

    new functions next_weekday and is_date

--- R/utils.R
+++ R/utils.R
@@ -13,3 +13,30 @@
 is_named_vector <- function(x, mode = "any") {
   is.vector(x, mode = mode) & !is.null(names(x)) & !any(is.na(names(x)))
 }
+
+#' next_weekday
+#'
+#' Choose the next weekday for a date
+#' @param x Date type.
+#' @keywords date weekday bizday
+#' @examples
+#' next_weekday(Sys.Date())
+#' @export
+
+next_weekday <- function(x) {
+  x <- x + 1
+  x + setNames(c(rep(0, 5), 2:1), 1:7)[format(x, "%u")]
+}
+
+#' is_date
+#'
+#' Check if variable is a date or a datetime object.
+#' @param x Date type.
+#' @keywords date weekday bizday
+#' @examples
+#' is_date(Sys.Date())
+#' @export
+
+is_date <- function(x) {
+  inherits(x, c("Date", "POSIXt"))
+}
