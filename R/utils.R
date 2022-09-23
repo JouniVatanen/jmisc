975d75d5e4dd6825b293d60e4e617477e27adf1f
Jouni Vatanen <jouni.vatanen@ilmarinen.fi>
2022-09-23 15:26:08 +0300

    new function xml_path2list is used mainly in questback api functions.

--- R/utils.R
+++ R/utils.R
@@ -40,3 +40,43 @@ next_weekday <- function(x) {
 is_date <- function(x) {
   inherits(x, c("Date", "POSIXt"))
 }
+
+#' xml_path2list
+#'
+#' Transforms xml xpath to list. Is used especially with Questback api functions.
+#' xml2 functions have some kind of problems with nested named XML lists.
+#' @param x XML file.
+#' @param xpath Path of the XML file.
+#' @keywords XML Questback
+#' @importFrom data.table data.table
+#' @importFrom xml2 xml_find_all
+#' @importFrom xml2 xml_length
+#' @importFrom xml2 xml_children
+#' @importFrom xml2 xml_text
+#' @importFrom xml2 xml_name
+#' @importFrom dplyr bind_rows
+#' @export
+
+xml_path2list <- function(x, xpath) {
+  xml <- xml2::xml_find_all(x, xpath)
+  if (all(xml2::xml_length(xml) == 0)) { return(data.table::data.table(NULL)) }
+  l <- lapply(xml, function(x) {
+    c <- xml2::xml_children(x)
+    len <- xml2::xml_length(c)
+    if (any(len == 0)) {
+      n_child <- setNames(xml2::xml_text(c[len == 0]), xml2::xml_name(c[len == 0]))
+    }
+    # list of childrens childrend
+    if (any(len > 0)) {
+      l_child <- list()
+      c_name <- xml2::xml_name(c[len != 0])
+      cc <- xml2::xml_children(c[len != 0])
+      cc_names <- xml2::xml_name(cc)
+      l_child[[c_name]] <- setNames(as_list(cc), cc_names)
+      out <-  append(l_child, n_child)
+    } else { out <- list(n_child) }
+    return(out)
+  })
+  dplyr::bind_rows(l)
+}
+
